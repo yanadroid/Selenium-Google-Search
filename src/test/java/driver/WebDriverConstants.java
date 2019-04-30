1a1209b2c9a843f2ab011f69bc4956bae841b5a3
yanadroid <yana.cvigun@gmail.com>
2019-04-30 21:13:57 +0300

    Chnaged interface

--- src/test/java/driver/WebDriverConstants.java
+++ src/test/java/driver/WebDriverConstants.java
@@ -2,6 +2,6 @@ package driver;
 
 public interface WebDriverConstants {
 
-     public static final String KEY_TAG_NAME = "cite";
-     public static final long KEY_TIME_WAIT = 5000;
+     String KEY_TAG_NAME = "cite";
+     long KEY_TIME_WAIT = 5000;
 }
